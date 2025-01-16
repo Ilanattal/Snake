9d7e95788f914464a027308f36ea15395ada4230
Ilanattal <i29attal.attal@gmail.com>
2025-01-16 18:16:59 +0200

    vite new

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Snake/', // Remplacez "snake" par le nom de votre repository GitHub
+  base: "/Snake/", // Nom exact de votre dépôt (sensible à la casse)
   plugins: [react()],
 });
